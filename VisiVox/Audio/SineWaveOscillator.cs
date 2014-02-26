37f7a24705d46bae92d62fc9b2fef8d7c1ef5c08
shwars <dmitri@soshnikov.com>
2014-02-27 00:25:06 +0400

    Initial Version

---
+++ VisiVox/Audio/SineWaveOscillator.cs
@@ -0,0 +1,32 @@
+﻿// SineWaveOscillator.cs by Charles Petzold, November 2009
+using System;
+using NAudio.Wave;
+
+namespace LeapVox.Audio
+{
+    class SineWaveOscillator : WaveProvider16
+    {
+        double phaseAngle;
+
+        public SineWaveOscillator(int sampleRate): 
+            base(sampleRate, 1)
+        {
+        }
+
+        public double Frequency { set; get; }
+        public short Amplitude { set; get; }
+
+        public override int Read(short[] buffer, int offset, int sampleCount)
+        {
+            for (int index = 0; index < sampleCount; index++)
+            {
+                buffer[offset + index] = (short)(Amplitude * Math.Sin(phaseAngle));
+                phaseAngle += 2 * Math.PI * Frequency / WaveFormat.SampleRate;
+
+                if (phaseAngle > 2 * Math.PI)
+                    phaseAngle -= 2 * Math.PI;
+            }
+            return sampleCount;
+        }
+    }
+}
---
+++ VisiVox/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Leap;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Speech.Synthesis;
+using LeapVox.Audio;
+using System.Windows.Media.Animation;
+
+namespace LeapVox
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PortamentoSineWaveOscillator osc;
+        WaveOut WaveOutput;
+
+        Controller Leap=null;
+        DispatcherTimer dt = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1 / 30) };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Leap = new Controller();
+
+            dt.Tick += dt_Tick;
+            dt.Start();
+
+            WaveOutput = new WaveOut();
+            osc = new PortamentoSineWaveOscillator(44100,120);
+            osc.Pitch = 120;
+            osc.Amplitude = 0;
+
+            WaveOutput.Init(osc);
+            WaveOutput.Play();
+
+            FirePS.Start();
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            using (var Frame = Leap.Frame())
+            {
+                var f = Frame.Fingers.FirstOrDefault();
+                if (f != null)
+                {
+                    var p = Math.Abs(f.TipPosition.y / 2);
+                    var a = 255 - Math.Abs(f.TipPosition.x);
+                    if (p >= 0 && p <= 150) osc.Pitch = p;
+                    if (a >= 0 && a <= 255) osc.Amplitude = (short)a;
+                    emit.X = f.TipPosition.x * 2 + 512;
+                    emit.Y = -f.TipPosition.y * 2 + 600;
+                }
+            }
+        }
+
+
+    }
+
+}
